a74cd28f821c5153131b1d30bb27321e1932ef41
Bivol Mihail <mihail.bivol@iongroup.com>
2025-10-13 17:23:04 +0300

    feat: searching, and show details

--- src/tableManager2.js
+++ src/tableManager2.js
@@ -3,27 +3,31 @@ const SERVER_URL =
   "large"
     ? "https://dummyjson.com/users?limit=208"
     : "https://dummyjson.com/users?limit=32";
-const ROWS = 50;
+const ROWS_PER_PAGE = 50;
 const PAGE_WINDOW_SIZE = 5;
 const FIRST_PAGE = 1;
+const ONE_PAGE = 1;
+const ZERO_PAGES = 0;
 
 class PaginatedDataSet {
   #rows;
   #dataSet;
   #numberOfPages;
   #currentPageNumber;
+  #filteredDataSet;
   constructor(dataSet, rows) {
     this.#dataSet = dataSet;
     this.#rows = rows;
-    this.#numberOfPages = Math.ceil(dataSet.length / rows);
     this.#currentPageNumber = FIRST_PAGE;
+    this.#filteredDataSet = [...this.#dataSet];
+    this.computeNumberOfPages();
   }
 
   getPageByNumber(pageNumber) {
     let start = (pageNumber - FIRST_PAGE) * this.#rows;
     let end = start + this.#rows;
 
-    return this.#dataSet.slice(start, end);
+    return this.#filteredDataSet.slice(start, end);
   }
 
   goToPage(pageNumber) {
@@ -46,6 +50,29 @@ class PaginatedDataSet {
   set currentPageNumber(pageNumber) {
     this.#currentPageNumber = pageNumber;
   }
+
+  computeNumberOfPages() {
+    this.#numberOfPages = Math.ceil(this.#filteredDataSet.length / this.#rows);
+  }
+
+  filter(text) {
+    text = text.toLowerCase();
+    this.#filteredDataSet = this.#dataSet.filter((entry) => {
+      return (
+        entry.id === Number(text) ||
+        entry.firstName.toLowerCase().includes(text) ||
+        entry.lastName.toLowerCase().includes(text) ||
+        entry.email.toLowerCase().includes(text) ||
+        entry.phone.toLowerCase().includes(text)
+      );
+    });
+    this.computeNumberOfPages();
+    this.#currentPageNumber = FIRST_PAGE;
+  }
+
+  getDataEntryById(id) {
+    return this.#filteredDataSet.find((entry) => entry.id === id);
+  }
 }
 
 async function fetchData() {
@@ -63,7 +90,9 @@ function routePageButtons(dataSet) {
   let wrapper = document.getElementById("page-button-wrapper");
   wrapper.addEventListener("click", (event) => {
     if (event.target.tagName === "BUTTON") {
-      const dataSetPage = dataSet.goToPage(Number(event.target.getAttribute("value")));
+      const dataSetPage = dataSet.goToPage(
+        Number(event.target.getAttribute("value"))
+      );
       renderPageTable(dataSetPage);
       renderPageButtons(dataSet.currentPageNumber, dataSet.numberOfPages);
     }
@@ -71,21 +100,28 @@ function routePageButtons(dataSet) {
 }
 
 function renderPageButtons(currentPageNumber, numberOfPages) {
-  console.log("currentPage:", currentPageNumber);
   let wrapper = document.getElementById("page-button-wrapper");
+  if (numberOfPages === ONE_PAGE || numberOfPages === ZERO_PAGES) {
+    wrapper.innerHTML = "";
+    return;
+  }
   let buttons = "";
 
   const halfWindow = Math.floor(PAGE_WINDOW_SIZE / 2);
   let start = Math.max(FIRST_PAGE, currentPageNumber - halfWindow);
-  let end = start + PAGE_WINDOW_SIZE - 1;
+  let end = start + PAGE_WINDOW_SIZE - ONE_PAGE;
 
   if (end > numberOfPages) {
     end = numberOfPages;
     start = Math.max(FIRST_PAGE, end - PAGE_WINDOW_SIZE + FIRST_PAGE);
   }
 
-  for (let page = start; end != FIRST_PAGE && page <= end; page++) {
-    buttons += `<button value=${page} class="page btn btn-lg btn-outline-dark mx-1">${page}</button>`;
+  for (let page = start; page <= end; page++) {
+    let activeButton = "";
+    if (page == currentPageNumber) {
+      activeButton = "active";
+    }
+    buttons += `<button value=${page} class="page btn btn-lg btn-outline-dark mx-1 ${activeButton}">${page}</button>`;
   }
 
   if (currentPageNumber !== FIRST_PAGE) {
@@ -116,18 +152,19 @@ function renderPageTable(dataSetPage) {
 }
 
 function routeSortingIndicators(dataSet) {
-  document.querySelectorAll("th[data-column]").forEach((th) => {
-    th.addEventListener("click", () => {
-      const column = th.getAttribute("data-column");
-      let order = th.getAttribute("data-order");
+  const tableHeader = document.getElementById("table-head");
 
-      order = order === "desc" ? "asc" : "desc";
-      th.setAttribute("data-order", order);
-      let dataSetPage = [...dataSet.getCurrentPage()];
-      dataSetPage.sort((a, b) => compareBy(a, b, column, order));
-      updateSortingIndicators(th, order);
-      renderPageTable(dataSetPage);
-    });
+  tableHeader.addEventListener("click", (event) => {
+    const th = event.target.closest("th");
+    const column = th.getAttribute("data-column");
+    let order = th.getAttribute("data-order");
+
+    order = order === "desc" ? "asc" : "desc";
+    th.setAttribute("data-order", order);
+    let dataSetPage = [...dataSet.getCurrentPage()];
+    dataSetPage.sort((a, b) => compareBy(a, b, column, order));
+    updateSortingIndicators(th, order);
+    renderPageTable(dataSetPage);
   });
 }
 
@@ -145,19 +182,60 @@ function compareBy(a, b, column, order) {
 function updateSortingIndicators(th, order) {
   let upArrow = "&uarr;";
   let downArrow = "&darr;";
-  let oldHtml = th.innerHTML;
+  let oldText = th.textContent.trim();
   if (order === "asc") {
-    th.innerHTML = oldHtml.slice(0, -1) + upArrow;
+    th.innerHTML = oldText.slice(0, -1) + upArrow;
   } else {
-    th.innerHTML = oldHtml.slice(0, -1) + downArrow;
+    th.innerHTML = oldText.slice(0, -1) + downArrow;
   }
 }
 
+function routeSearchBar(dataSet) {
+  const searchBar = document.getElementById("search-bar");
+  const searchButton = document.getElementById("search-button");
+
+  searchButton.addEventListener("click", () => {
+    let searchText = searchBar.value;
+    dataSet.filter(searchText);
+    renderPageTable(dataSet.getCurrentPage());
+    renderPageButtons(dataSet.currentPageNumber, dataSet.numberOfPages);
+  });
+}
+
+function renderDetails(data) {
+  const details = document.getElementById("details-container");
+  details.innerHTML = `<p>User selected <b>${data.firstName} ${data.lastName}</b></p>
+                       <p>Description:</p>
+                       <textarea>age: ${data.age}\ngender: ${data.gender}\ndate-of-birth: ${data.birthDate}</textarea>
+                       <p>Residential address: <b>${data.address.address}</b></p>
+                       <p>City: <b>${data.address.city}</b></p>
+                       <p>Province/State: <b>${data.address.state}</b></p>
+                       <p>Index <b>${data.address.postalCode}</b></p>`;
+}
+
+function routeTableRows(dataSet) {
+  document.getElementById("data-table").addEventListener("click", (event) => {
+    const tr = event.target.closest("tr");
+    if (tr === null) {
+      return;
+    }
+    console.log("TR", tr);
+    console.log("TR CHILDREND", tr.children);
+    const id = Number(tr.children[0].textContent);
+    console.log("ID", id);
+    const entry = dataSet.getDataEntryById(id);
+    console.log("ENTRY", entry);
+    renderDetails(entry);
+  });
+}
+
 fetchData().then((data) => {
-  const dataSet = new PaginatedDataSet(data, ROWS);
+  const dataSet = new PaginatedDataSet(data, ROWS_PER_PAGE);
   let firstPage = dataSet.getCurrentPage();
   renderPageTable(firstPage);
+  routeTableRows(dataSet);
   renderPageButtons(dataSet.currentPageNumber, dataSet.numberOfPages);
   routePageButtons(dataSet);
   routeSortingIndicators(dataSet);
+  routeSearchBar(dataSet);
 });
